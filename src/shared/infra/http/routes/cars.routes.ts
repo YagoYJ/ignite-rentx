423d15d3b29093fc5021d48c1be24af5f7e28285
yago.juan <yago.juan@teclat.com.br>
2022-07-25 11:29:49 -0300

    Upload Cars Images

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { ensureAdmin } from "../middlewares/ensureAdmin";
-import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
+
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig);
 
 carsRoutes.post(
   "/",
@@ -18,6 +26,8 @@ carsRoutes.post(
   createCarController.handle
 );
 
+carsRoutes.get("/available", listAvailableCarsController.handle);
+
 carsRoutes.post(
   "/specifications/:id",
   ensureAuthenticated,
@@ -25,6 +35,12 @@ carsRoutes.post(
   createCarSpecificationController.handle
 );
 
-carsRoutes.get("/available", listAvailableCarsController.handle);
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array("images"),
+  uploadCarImagesController.handle
+);
 
 export { carsRoutes };
